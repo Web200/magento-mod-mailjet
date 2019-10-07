2edb1ae6d492197d77ae5e82b31bebb4d6d07ad1
Pierre <pierre.basile@gmail.com>
2019-10-07 23:11:55 +0200

    Add Mailjet Template in default template list

---
+++ Plugin/EmailTemplate.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Web200\Mailjet\Plugin;
+
+use Web200\Mailing\Model\Source\EmailTemplate as BaseEmailTemplate;
+use Web200\Mailjet\Model\Config as MailjetConfig;
+use Web200\Mailjet\Model\Config\Source\Template as MailjetTemplate;
+
+/**
+ * Class EmailTemplate
+ *
+ * @category    Class
+ * @package     Web200\Mailjet\Plugin
+ * @author      Web200 Team <contact@web200.fr>
+ * @copyright   Web200
+ * @license     https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link        https://www.web200.fr/
+ */
+class EmailTemplate
+{
+    /**
+     * @var MailjetConfig
+     */
+    protected $mailjetConfig;
+    /**
+     * @var MailjetTemplate
+     */
+    protected $mailjetTemplate;
+
+    /**
+     * EmailTemplatePlugin constructor.
+     *
+     * @param MailjetConfig   $mailjetConfig
+     * @param MailjetTemplate $mailjetTemplate
+     */
+    public function __construct(
+        MailjetConfig $mailjetConfig,
+        MailjetTemplate $mailjetTemplate
+    ) {
+        $this->mailjetConfig   = $mailjetConfig;
+        $this->mailjetTemplate = $mailjetTemplate;
+    }
+
+    /**
+     * Add Mailjet Templates
+     *
+     * @param BaseEmailTemplate $emailTemplate
+     * @param                   $templates
+     * @return mixed
+     */
+    public function afterToOptionArray(BaseEmailTemplate $emailTemplate, $templates): array
+    {
+        if ($this->mailjetConfig->active()) {
+            $templates = $this->getMailjetTemplates($templates);
+        }
+        return $templates;
+    }
+
+    /**
+     * Get Mailjet Templates
+     *
+     * @param array $templates
+     * @return array
+     */
+    protected function getMailjetTemplates($templates): array
+    {
+        $mailjetTemplates = $this->mailjetTemplate->toOptionArray();
+        foreach ($mailjetTemplates as $mailjetTemplate) {
+            $templates[] = [
+                'value' => 'mailjet_' . $mailjetTemplate['value'],
+                'label' => '[Mailjet] ' . $mailjetTemplate['label'],
+            ];
+        }
+        return $templates;
+    }
+}
